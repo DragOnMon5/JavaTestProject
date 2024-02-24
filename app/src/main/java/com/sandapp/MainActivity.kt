4520295751f0078927bcf832204ab28cfa5024e6
DragOnMon5 <shogenov.vlad@mail.ru>
2024-02-24 18:43:34 +0300

    add button with image

--- app/src/main/java/com/sandapp/MainActivity.kt
+++ app/src/main/java/com/sandapp/MainActivity.kt
@@ -3,34 +3,47 @@ package com.sandapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.sandapp.ui.theme.SAndAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SAndAppTheme {
+
+        /*setContent {
+            //SAndAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Throughout Heaven and Earth...")
+
+                    Text(text = "check", fontSize = 25.em)
+                    //Print("Throughout Heaven and Earth...")
+                    //GreetingPreview()
                 }
-            }
-        }
+            //}
+        }*/
+
+        setContentView(R.layout.activity_main_screen)
+
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Print(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "$name",
         modifier = modifier
@@ -41,6 +54,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SAndAppTheme {
-        Greeting("Android")
+        Print("Android")
     }
 }
---
+++ app/src/main/java/com/sandapp/activity_main_screen.kt
@@ -0,0 +1,24 @@
+package com.sandapp
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+
+class activity_main_screen : AppCompatActivity() {
+
+    private lateinit var mHelloTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+
+
+        var imageButton: ImageButton = findViewById(R.id.imageButton)
+        var numberOfClicks: Int = 0
+        /*imageButton.setOnKeyListener{
+            imageButton.setBackgroundColor(true)
+        }*/
+    }
+}
