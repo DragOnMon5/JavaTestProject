7bdeaa539a280edb75a9604967073eb9b5df5167
DragOnMon5 <shogenov.vlad@mail.ru>
2024-02-26 20:08:02 +0300

    add View Binding and few bugs fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,11 @@ plugins {
 }
 
 android {
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     namespace = "com.sandapp"
     compileSdk = 34
 
--- app/src/main/java/com/sandapp/MainActivity.kt
+++ app/src/main/java/com/sandapp/MainActivity.kt
@@ -1,43 +1,52 @@
 package com.sandapp
 
-import android.content.pm.ActivityInfo
+import  android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sandapp.databinding.ActivityMainScreenBinding
 import com.sandapp.ui.theme.SAndAppTheme
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+import kotlin.math.round
 
 
 class MainActivity : ComponentActivity() {
-    var screen_id = R.layout.activity_main_screen
+    lateinit var binding: ActivityMainScreenBinding
+    var score:Float = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(screen_id)
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
-        var imageButton: ImageButton = findViewById(R.id.imageButton)
-        var numberOfClicks: Int = 0
-
-        imageButton.setOnClickListener { onPurpleClick(imageButton) }
-
+        // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        binding = ActivityMainScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.imageButton.setOnClickListener { onPurpleClick() }
     }
 
-    fun onPurpleClick(imageButton: ImageButton) {
-        imageButton.rotation += 2
-        imageButton.scaleX += 0.04f
-        imageButton.scaleY += 0.04f
-        if (imageButton.scaleX >= 5f) {
-            changeLayout()
+    fun onPurpleClick() {
+        binding.imageButton.rotation += 2
+        binding.imageButton.scaleX += 0.04f
+        binding.imageButton.scaleY += 0.04f
+        score = round((5 - binding.imageButton.scaleX) * 100) / 100
+        changeScore(score.toString())
+        if (binding.imageButton.scaleX >= 5f) {
+            changeLayout(R.layout.side_screen)
         }
     }
 
-    fun changeLayout() {
-        setContentView(R.layout.side_screen)
+    fun changeLayout(screen_id : Int) {
+        setContentView(screen_id)
+    }
+
+    fun changeScore(newScore: String) {
+        binding.textScore.text = newScore
     }
 }
-
 
 @Composable
 fun Print(name: String, modifier: Modifier = Modifier) {
